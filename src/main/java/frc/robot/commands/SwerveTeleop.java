8b5edec46a4aad11ffd9983e9a25f2fc9dd7cdf1
phl_44 <phatharaguchi3@gmail.com>
2026-02-28 16:02:05 -1000

    Fix Teleop Limiter

--- src/main/java/frc/robot/commands/SwerveTeleop.java
+++ src/main/java/frc/robot/commands/SwerveTeleop.java
@@ -8,12 +8,12 @@ import java.util.ResourceBundle.Control;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.fasterxml.jackson.databind.util.LRUMap;
-import com.pathplanner.lib.util.DriveFeedforwards;
 import com.ctre.phoenix6.swerve.SwerveModule;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -74,31 +74,51 @@ public class SwerveTeleop extends Command {
         rInput = -controller.getRightX();
 
         setPolynomialAcceleration();
-
+        phatSpeed(); // Apply slew rate limiting to the speeds
         drivetrain.applyRequest(() -> drive.withVelocityX(ySpeed)
             .withVelocityY(xSpeed)
             .withRotationalRate(rSpeed)).execute();
-
-        setDashboardData();
     }
     
     // Apply a polynomial acceleration curve to the joystick inputs for smoother control
     private void setPolynomialAcceleration() {
-        xSpeed = Utilities.polynomialAccleration(yInput) * MaxSpeed * 0.3;
-        ySpeed = Utilities.polynomialAccleration(xInput) * MaxSpeed * 0.3;
-        rSpeed = Utilities.polynomialAccleration(rInput) * MaxAngularRate * 0.7 ; 
+        xSpeed = Utilities.polynomialAccleration(yInput) * MaxSpeed * 0.5;
+        ySpeed = Utilities.polynomialAccleration(xInput) * MaxSpeed * 0.5;
+        rSpeed = Utilities.polynomialAccleration(rInput) * MaxAngularRate * 0.5; 
     }
+    
+    // Limit the rate of change of the xSpeed to 0.8 m/s^2
+    private final SlewRateLimiter xLimiter = new SlewRateLimiter(0.8);
+    private final SlewRateLimiter yLimiter = new SlewRateLimiter(0.8);
+    private final SlewRateLimiter rLimiter = new SlewRateLimiter(0.8); 
 
+    //My methods of the code 
+    private void phatSpeed() {
+    xSpeed = xLimiter.calculate(xSpeed); //Apply slew rate limiting to the xSpeed
+    ySpeed = yLimiter.calculate(ySpeed);
+    rSpeed = rLimiter.calculate(rSpeed);
+}
     private void autoYaw(boolean Enable) {
         c_yawPID.setSetpoint(0);
     }
 
     private void setDashboardData() {
-        SmartDashboard.putNumber(drivetrain.getName() + " pidgeon 2", Utilities.processYaw(drivetrain.getPigeon2().getYaw().getValueAsDouble()));
-        SmartDashboard.putNumber(drivetrain.getName() + " state x pos", drivetrain.getState().Pose.getMeasureX().baseUnitMagnitude());
-        SmartDashboard.putNumber(drivetrain.getName() + " state y pos", drivetrain.getState().Pose.getMeasureY().baseUnitMagnitude());
-        SmartDashboard.putNumber(drivetrain.getName() + " state rot pos", drivetrain.getState().Pose.getRotation().getDegrees());
-        SmartDashboard.putNumber(drivetrain.getName() + " yaw diff", 
-            drivetrain.getPigeon2().getYaw().getValueAsDouble() - Utilities.processYaw(drivetrain.getState().Pose.getRotation().getDegrees()));
+        SmartDashboard.putNumber("xSpeed", xSpeed);
+        SmartDashboard.putNumber("ySpeed", ySpeed);
+        SmartDashboard.putNumber("rSpeed", rSpeed);
+        SmartDashboard.putNumber("xInput", xInput);
+        SmartDashboard.putNumber("yInput", yInput);
+        SmartDashboard.putNumber("rInput", rInput);
+        SmartDashboard.putNumber(getName(), MaxAngularRate);
+        SmartDashboard.putNumber(getName(), MaxSpeed);
+        SmartDashboard.putNumber(getName(), deadband);
+        SmartDashboard.putNumber(getName(), c_yawPID.getP());
+        SmartDashboard.putNumber(getName(), c_yawPID.getI()); 
+        SmartDashboard.putNumber(getName(), c_yawPID.getD());
+        SmartDashboard.putData("xLimiter", (Sendable) xLimiter);
+        SmartDashboard.putData("yLimiter", (Sendable) yLimiter);
+        SmartDashboard.putData("rLimiter", (Sendable) rLimiter);
+        SmartDashboard.putBoolean(getName(), true);
+        // TODO add data to dashboard for testing and debugging purposes
     }
 }
